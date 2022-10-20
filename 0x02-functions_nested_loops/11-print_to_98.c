535b78ec92db985b6e2786d8fbc8fdbe13374147
Emmanuel Onyedika <sichogenonline@gmail.com>
2022-10-19 18:36:23 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_t0_98 - print all numbers from input to 98
+ * print_to_98 - print all numbers from input to 98
  * @n: the starting number
  */
 void print_to_98(int n)
